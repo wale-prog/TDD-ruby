2a590adacd0da9266eecf7b4b191f83db55f6fde
Olawale <olapetanwale@outlook.com>
2022-09-03 16:08:30 +0100

    Add test for reverse method

--- solver.rb
+++ solver.rb
@@ -1,5 +1,12 @@
 class Solver
   def factorial(num)
-   
+    if num < 0
+      raise ArgumentError.new('Expected input argument to be a non-negative integer')
+    end
+    (1..num).reduce(:*) || 1
+  end
+
+  def reverse(string)
+    
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,16 +1,28 @@
 require_relative '../solver'
 
 describe Solver do
-	describe '#factorial' do
-		solver = Solver.new
+	before(:all) do
+		@solver = Solver.new
+	end
+
+	describe '#factorial' do		
   	it "should return argument error if num is negative" do
-			expect { solver.factorial(-2) }.to raise_error(ArgumentError)
+			expect { @solver.factorial(-2) }.to raise_error(ArgumentError)
 		end
-		it "should return 1 whne input argument is 1" do 
-			expect(solver.factorial(1)).to eql(1)
+		it "should return 1 when input argument is 1" do 
+			expect(@solver.factorial(1)).to eql(1)
 		end
 		it "should return 120 when input argument is 5" do
-			expect(solver.factorial(5)).to eq(120)
+			expect(@solver.factorial(5)).to eq(120)
+		end
+	end
+
+	describe '#reverse' do
+		it "should return drow when the input argument is word" do
+			expect(@solver.reverse('word')).to eq('drow')
+		end
+		it "should return olleh when the input argument is hello" do
+			expect(@solver.reverse('hello')).to eq('olleh')
 		end
 	end
 end
